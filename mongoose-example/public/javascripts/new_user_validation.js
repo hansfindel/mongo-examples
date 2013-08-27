c9abaa305516c0d2a93bb2496a8b30064760f44b
Hans <hanstrax@gmail.com>
2013-08-26 21:08:50 -0400

    mongoose example client-js update

---
+++ mongoose-example/public/javascripts/new_user_validation.js
@@ -0,0 +1,26 @@
+function getSubmitButton(){
+	return document.getElementById("newUserSubmit").childNodes[0];
+}
+function check(e){
+	e.preventDefault();
+	var missing = []	
+	divs = document.getElementsByClassName("field")	
+	for(var i = 0; i < divs.length; i++){
+		var div = divs[i]
+		label = div.childNodes[0]
+		input = div.childNodes[1]
+		if(input.value==""){
+			var name = label.textContent
+			missing.push(name)
+		}
+	}
+	if(missing.length > 0){
+		alert("Debes rellenar todos los campos. \nRevisa: " + missing.join(", ") )
+		return false;			
+	}else{
+		return true;
+	}
+}
+
+getSubmitButton().onclick = check;
+getSubmitButton().onsubmit = check;
